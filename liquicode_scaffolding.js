9bd400e81a6a4e44bd5e470c8a5970a26201e27f
agbowlin <agbowlin@gmail.com>
2017-10-14 00:56:13 +0000

    Added install and update modes.

--- liquicode_scaffolding.js
+++ liquicode_scaffolding.js
@@ -56,6 +56,15 @@ const usage_definitions = [{
 		typeLabel: '[underline]{path}'
 	},
 	{
+		name: 'install',
+		description: 'Force installation mode.' +
+			'By default, core files are always copied but app files ' +
+			'are copied only if they don\'t exist.',
+		alias: 'i',
+		type: Boolean,
+		typeLabel: '[underline]{bool}'
+	},
+	{
 		name: 'help',
 		description: 'Print this help.',
 		alias: 'h',
@@ -153,14 +162,51 @@ if (app_options.targetpath) {
 
 console.log('Checking target [' + targetpath + ']');
 npm_fs_extra.ensureDirSync(targetpath);
+npm_fs_extra.ensureDirSync(npm_path.join(targetpath, 'client'));
+npm_fs_extra.ensureDirSync(npm_path.join(targetpath, 'client/partials'));
+npm_fs_extra.ensureDirSync(npm_path.join(targetpath, 'server'));
 
 
 //---------------------------------------------------------------------
 //	Copy the source files to the target
 //---------------------------------------------------------------------
 
-console.log('Copying files');
-npm_fs_extra.copySync(sourcepath, targetpath, { "preserveTimestamps": true });
+function copy_file(Source, Target, Overwrite) {
+	if (npm_fs_extra.existsSync(Target)) {
+		if (!Overwrite) {
+			console.log('[' + Target + '] already exists, skipping');
+		}
+		else {
+			console.log('[' + Target + '] overwriting');
+			npm_fs_extra.copySync(Source, Target, { "preserveTimestamps": true });
+		}
+	}
+	else {
+		console.log('[' + Target + '] copying');
+		npm_fs_extra.copySync(Source, Target, { "preserveTimestamps": true });
+	}
+}
+
+// npm_fs_extra.copySync(sourcepath, targetpath, { "preserveTimestamps": true });
+
+console.log('Copying Server Files');
+copy_file(npm_path.join(sourcepath, 'server/server.js'), npm_path.join(targetpath, 'server/server.js'), true);
+copy_file(npm_path.join(sourcepath, 'server/package.json'), npm_path.join(targetpath, 'server/package.json'), app_options.install);
+copy_file(npm_path.join(sourcepath, 'server/app-server.js'), npm_path.join(targetpath, 'server/app-server.js'), app_options.install);
+
+console.log('Copying Client Files');
+copy_file(npm_path.join(sourcepath, 'client/index.css'), npm_path.join(targetpath, 'client/index.css'), true);
+copy_file(npm_path.join(sourcepath, 'client/index.html'), npm_path.join(targetpath, 'client/index.html'), true);
+copy_file(npm_path.join(sourcepath, 'client/index.js'), npm_path.join(targetpath, 'client/index.js'), true);
+copy_file(npm_path.join(sourcepath, 'client/bower.json'), npm_path.join(targetpath, 'client/bower.json'), app_options.install);
+copy_file(npm_path.join(sourcepath, 'client/package.json'), npm_path.join(targetpath, 'client/package.json'), app_options.install);
+copy_file(npm_path.join(sourcepath, 'client/app-client.css'), npm_path.join(targetpath, 'client/app-client.css'), app_options.install);
+copy_file(npm_path.join(sourcepath, 'client/app-client.html'), npm_path.join(targetpath, 'client/app-client.html'), app_options.install);
+copy_file(npm_path.join(sourcepath, 'client/app-client.js'), npm_path.join(targetpath, 'client/app-client.js'), app_options.install);
+copy_file(npm_path.join(sourcepath, 'client/partials/member-login.html'), npm_path.join(targetpath, 'client/partials/member-login.html'), app_options.install);
+copy_file(npm_path.join(sourcepath, 'client/partials/member-profile.html'), npm_path.join(targetpath, 'client/partials/member-profile.html'), app_options.install);
+copy_file(npm_path.join(sourcepath, 'client/partials/member-signup.html'), npm_path.join(targetpath, 'client/partials/member-signup.html'), app_options.install);
+copy_file(npm_path.join(sourcepath, 'client/partials/site-home.html'), npm_path.join(targetpath, 'client/partials/site-home.html'), app_options.install);
 
 
 //---------------------------------------------------------------------
